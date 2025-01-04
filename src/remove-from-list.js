eb2162f90cbc04495b63c96110288b8de19c4f57
mikegrekov <svg200914@gmail.com>
2025-01-04 07:32:07 +0000

    feat: implement remove from tree task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let head = l;
+  let curr = l;
+  let prevNode = null;
+  while (curr) {
+    let nextNode = curr.next;
+    if (curr.value === k && !prevNode) {
+      head = nextNode;
+      curr = nextNode;
+      continue;
+    } else if (curr.value === k && prevNode) {
+      prevNode.next = nextNode;
+      curr = nextNode;
+      continue;
+    } 
+    prevNode = curr;
+    curr = nextNode;
+  }
+  return head;
 }
 
 module.exports = {
